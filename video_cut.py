70df538f21d10e7bf7b302757c3c044710eabb6a
kimoto22 <n22m305@matsu.shimane-u.ac.jp>
2023-10-20 21:10:31 +0900

    フォルダ構成の編集とパスの編集

--- video_cut.py
+++ video_cut.py
@@ -1,58 +1,92 @@
-from moviepy.editor import VideoFileClip
 import cv2
-import datetime
+import os
+from datetime import datetime, timedelta
+import pandas as pd
 
 # 対象者の名前
 subject_name = "goto"
 
-# 切り取られたビデオファイルの名前
-cut_video_name = "NotFocus"
+# 動画ファイルのパスを構築
+file_path = f".\\SearcTubeResultsAPP\\subjects\\{subject_name}\\{subject_name}_PCカメラ映像.avi"
+output_txt_path = f".\\SearcTubeResultsAPP\\sbjects\\{subject_name}\\{subject_name}_FrameTime.txt"
+csv_file_path = f".\\SearcTubeResultsAPP\\subjects\\{subject_name}\\{subject_name}.csv"
+csv_data = pd.read_csv(csv_file_path)
 
-# CSV タイムスタンプログファイルとビデオファイルを指定
-input_video_path = f".\\SearcTubeResultsAPP\\{subject_name}\\{subject_name}_PCカメラ映像.avi"
-output_video_path = f".\\SearcTubeResultsAPP\\{subject_name}\\{subject_name}_{cut_video_name}.mp4"
 
-# 手動で指定する開始時刻と終了時刻
-start_time_str = '15:43:43'
-end_time_str = '15:46:50'
+def output_HMS(time_str):
+    time = datetime.strptime(time_str, "%H:%M:%S.%f")
+    HMS_time = time.strftime("%H:%M:%S.%f")
+    HMS_time_without_milliseconds = HMS_time.split(".")[0]
+    return HMS_time_without_milliseconds
 
-# 時刻文字列をdatetimeオブジェクトに変換します
-start_time = datetime.datetime.strptime(start_time_str, '%H:%M:%S')
-end_time = datetime.datetime.strptime(end_time_str, '%H:%M:%S')
 
-# 入力動画ファイルを開きます
-cap = cv2.VideoCapture(input_video_path)
+def read_csv(csv_data, frame_time):
+    start_time_str = "15:39:42.127503"
+    end_time_str = "15:43:31.916475"
 
-# 入力動画のフレームレートを取得します
-fps = int(cap.get(cv2.CAP_PROP_FPS))
+    HMS_start_time = output_HMS(start_time_str)
+    HMS_start_time = output_HMS(end_time_str)
+    HMS_frame_time = output_HMS(frame_time)
 
-# 出力動画の設定
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 出力動画のコーデックを指定します
-out = cv2.VideoWriter(output_video_path, fourcc, fps,
-                      (int(cap.get(3)), int(cap.get(4))))
+    # Timestamp列の各アイテムを取り出す
+    for index, timestamp_item in csv_data['Timestamp'].iteritems():
+        HMS_timestamp = output_HMS(timestamp_item)
 
-# 指定した時刻の前にシークします
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+        print(HMS_start_time, HMS_timestamp, HMS_frame_time)
 
-    # 現在のフレームの時刻を取得します
-    current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+        if HMS_start_time == HMS_timestamp and HMS_start_time == HMS_frame_time:
+            print(HMS_start_time)
 
-    # 指定した時刻の前に到達したら、ループを抜けます
-    if current_time >= (start_time.hour * 3600 + start_time.minute * 60 + start_time.second):
-        break
 
-# 指定した時刻から指定した時刻までのフレームを書き込みます
-while True:
-    ret, frame = cap.read()
-    if not ret or current_time >= (end_time.hour * 3600 + end_time.minute * 60 + end_time.second):
-        break
-    out.write(frame)
-    current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+def get_video_info(video_path):
+    # OpenCVを使用して動画の情報を取得
+    cap = cv2.VideoCapture(video_path)
 
-# クリーンアップ
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    # 動画の総フレーム数を取得
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # 動画のFPS（フレームレート）を取得
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # 動画の総時間を計算
+    total_seconds = total_frames / fps
+
+    # 動画の作成時刻と終了時刻を計算
+    creation_time = datetime.fromtimestamp(os.path.getctime(video_path))
+    end_time = creation_time + timedelta(seconds=total_seconds)
+
+    # フォーマットして表示
+    print(f"動画作成時刻: {creation_time}")
+    print(f"動画終了時刻: {end_time}")
+
+    # キャプチャを解放
+    cap.release()
+
+
+def get_frame_times(video_path, output_txt_path):
+    # OpenCVを使用して動画の情報を取得
+    cap = cv2.VideoCapture(video_path)
+
+    # 動画のFPS（フレームレート）を取得
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # ファイルを書き込みモードで開く
+    with open(output_txt_path, 'w') as file:
+        # 各フレームの時刻を取得して書き込む
+        for frame_number in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+            # フレーム番号から時刻を計算
+            current_time = datetime.fromtimestamp(os.path.getctime(video_path))
+            frame_time = current_time + timedelta(seconds=frame_number / fps)
+            read_csv(csv_data, frame_time)
+
+            # フォーマットして書き込み
+            file.write(f"Frame {frame_number + 1}: {frame_time}\n")
+
+    # キャプチャを解放
+    cap.release()
+
+
+# 動画の情報を表示
+get_video_info(file_path)
+# 動画の各フレームの時刻をテキストファイルに出力
+get_frame_times(file_path, output_txt_path)
